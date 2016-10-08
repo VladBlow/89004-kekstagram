c4b5405cf14b37196a4b4da6914caa9a66f81432
VladBlow <vladimir.tsibizow@gmail.com>
2016-10-09 01:01:46 +0300

    fixes

--- src/js/check.js
+++ src/js/check.js
@@ -2,39 +2,33 @@
 
 var getMessage = function (a, b) {
 
-  switch (typeof a) {
-    case 'boolean':
+  if (typeof a == 'boolean') {
+    if (a) {
+      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+    } else {
+      return "Переданное GIF-изображение не анимировано";
+    }
+  }
 
-      if (a == true) {
-        return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-      } else {
-        return "Переданное GIF-изображение не анимировано";
-      }
-      break;
+  if (typeof a == 'number') {
+    return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+  }
 
-    case 'number':
-      return "Переданное SVG-изображение содержит " + a + " объектов и " + (b * 4) + " атрибутов";
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0;
+    for (var i = 0; i < a.length; i++) {
+      artifactsSquare += a[i] * b[i];
+    }
+    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+  } else if (Array.isArray(a)){
+    var amountOfRedPoints = 0;
+    for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints = amountOfRedPoints + a[i];
+    }
+    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+  }
 
-    case 'object':
-      if (typeof b == 'object') {
-        var artifactsSquare = 0;
-
-        for (var i = 0; i < a.length; i++) {
-            artifactsSquare += parseInt(a[i]) * parseInt(b[i]);
-        }
-
-        return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
-      } else {
-        var amountOfRedPoints = 0;
-
-        for (var i = 0; i < a.length; i++) {
-          amountOfRedPoints = amountOfRedPoints + parseInt(a[i]);
-        }
-        console.log(typeof a);
-        return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-      }
-    default:
-      return "Переданы некорректные данные";
-
+  if (a || typeof a == 'boolean' || typeof a == 'number' || Array.isArray(a) && Array.isArray(b) || Array.isArray(a) || true) {
+    return "Переданы некорректные данные";
   }
 }
