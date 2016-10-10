3e3f39d02a9f7c20348104eb13b901ac1dc668cc
VladBlow <vladimir.tsibizow@gmail.com>
2016-10-10 18:12:50 +0300

    add outline and text block

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -111,6 +111,31 @@
       // Координаты задаются от центра холста.
       this._ctx.drawImage(this._image, displX, displY);
 
+      // Оверлей
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.beginPath();
+      this._ctx.moveTo(-this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, this._container.height / 2);
+      this._ctx.lineTo(-this._container.width / 2, this._container.height / 2);
+
+      this._ctx.moveTo(-this._resizeConstraint.side / 2 - this._ctx.lineWidth,
+                 -this._resizeConstraint.side / 2 - this._ctx.lineWidth);
+      this._ctx.lineTo(this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2,
+                 -this._resizeConstraint.side / 2 - this._ctx.lineWidth);
+      this._ctx.lineTo(this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2,
+                 this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2);
+      this._ctx.lineTo(-this._resizeConstraint.side / 2 - this._ctx.lineWidth,
+                 this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2);
+      this._ctx.closePath();
+      this._ctx.fill('evenodd');
+
+      // Текст
+      this._ctx.font = '20px sans-serif';
+      this._ctx.fillStyle = '#fff';
+      this._ctx.fillText(this._image.naturalWidth + ' x ' + this._image.naturalHeight, -this._container.width / 8.2, -this._container.width / 3.3);
+      console.log(this._container.width);
+
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
       this._ctx.strokeRect(
